ed87abad3846ac5276459ff0ca12835118cc16ed
Vasilis Dimakopoulos <83086659+vdimakopoulos@users.noreply.github.com>
2022-03-31 10:27:34 +0200

    Delete flip_trials_time.m

--- Subfunctions/flip_trials_time.m
+++
@@ -1,9 +0,0 @@
-function flipped_data = flip_trials_time(data)
-
-nTrials = length(data.trial)'
-flipped_data = data;
-for i = 1:nTrials
-    flipped_data.trial{i} = fliplr(data.trial{i})
-end
-
-end
