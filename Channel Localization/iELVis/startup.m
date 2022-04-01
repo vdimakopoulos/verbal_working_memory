cc855bc9636f0034b9eb2352f21e8193903b6970
Vasilis Dimakopoulos <83086659+vdimakopoulos@users.noreply.github.com>
2022-04-01 17:11:40 +0200

    Delete Channel Localization/iELVis directory

--- Channel Localization/iELVis/startup.m
+++
@@ -1,4 +0,0 @@
-strPaths.Toolboxes.iElVis = 'F:\Vasileios\Toolboxes\iElVis\';
-addpath(genpath(strPaths.Toolboxes.iElVis));
-global globalFsDir;
-globalFsDir='F:\Vasileios\Task Analysis\Data\Freesurfer Data\';
